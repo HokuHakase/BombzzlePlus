27ff3fa294d406336155d9d859e118c017162431
HokuHakase <hokuhakase@gmail.com>
2021-04-30 02:08:29 -0400

    First version of tile management and display through a Tilemap

---
+++ Scripts/BoardManager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var board = [
+			[0,1,2,3],
+			[1,2,3,4],
+			[2,3,4,0],
+			[3,4,0,1]
+			]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Scripts/GameBoard.gd
@@ -0,0 +1,19 @@
+extends TileMap
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# set_cell(1, 1, 2)
+	print(BoardManager.board.size())
+	for i in range(BoardManager.board.size()):
+		for j in range(BoardManager.board[i].size()):
+			set_cell(i+1, j+1, BoardManager.board[i][j])
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
